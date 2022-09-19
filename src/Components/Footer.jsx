ed38858cb6c75f54fb698258da6e4e60c3eff9ac
harisrahan9249 <harisrahman9249@gmail.com>
2022-09-19 23:07:07 +0200

    footer design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div>
+    <div className='foot'>
       <footer classNameName="footer-section">
         <div className="container">
             <div className="footer-cta pt-5 pb-5">
@@ -12,8 +12,8 @@ function Footer() {
                         <div className="single-cta">
                             <i className="fas fa-map-marker-alt"></i>
                             <div className="cta-text">
-                                <h4>Find us</h4>
-                                <span>1010 Avenue, sw 54321, chandigarh</span>
+                                <h4>Current Address</h4>
+                                <span>1kolej Na Větrníku, Za Zahradou, 162 00 Prague, Czechia</span>
                             </div>
                         </div>
                     </div>
@@ -21,16 +21,17 @@ function Footer() {
                         <div className="single-cta">
                             <i className="fas fa-phone"></i>
                             <div className="cta-text">
-                                <h4>Call us</h4>
-                                <span>9876543210 0</span>
+                                <h4>Phone</h4>
+                                <span>+420 777879716</span>
+                            </div>
+                            <div className="cta-text">
+                              
+                                <span>+420 777879716</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="far fa-envelope-open"></i>
-                            
-                        </div>
+                       
                     </div>
                 </div>
             </div>
@@ -41,50 +42,15 @@ function Footer() {
                             <div className="footer-logo">
                                 {/* <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo"/></a> */}
                             </div>
-                            <div className="footer-text">
-                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
-                                elit,Lorem ipsum dolor sit amet.</p>
-                            </div>
                             <div className="footer-social-icon">
-                                <span>Follow us</span>
-                                {/* <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
-                                <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
-                                <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a> */}
+                                <span>Follow </span>
+                                <a href="https://www.facebook.com/shine.karoth/"><img className="fab fa-facebook-f facebook-bg" src='./Icons/icons8-facebook-circled.svg'/></a>
+                                <a href="https://www.instagram.com/Shine_Karoth_"><img className="fab fa-facebook-f facebook-bg" src='./Icons/Instagram_icon.png.webp'/></a>
+                                <a href="https://www.linkedin.com/in/shine-karoth-3963a5211/"><img className="fab fa-facebook-f facebook-bg" src='./Icons/LinkedIn_icon_circle.svg.png'/></a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                        <div className="footer-widget">
-                            <div className="footer-widget-heading">
-                                <h3>Useful Links</h3>
-                            </div>
-                            {/* <ul>
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">about</a></li>
-                                <li><a href="#">services</a></li>
-                                <li><a href="#">portfolio</a></li>
-                                <li><a href="#">Contact</a></li>
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Our Services</a></li>
-                                <li><a href="#">Expert Team</a></li>
-                                <li><a href="#">Contact us</a></li>
-                                <li><a href="#">Latest News</a></li>
-                            </ul> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                        <div className="footer-widget">
-                            <div className="footer-widget-heading">
-                                <h3>Subscribe</h3>
-                            </div>
-                            <div className="footer-text mb-25">
-                                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
-                            </div>
-                            <div className="subscribe-form">
-                                
-                            </div>
-                        </div>
-                    </div>
+                 
                 </div>
             </div>
         </div>
